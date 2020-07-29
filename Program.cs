b43f141e149637b6adbafadd9af85ccd01a9c74a
Greg Olsen <gregolsen701@gmail.com>
2020-07-29 13:32:49 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab4_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days = new string[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            string[] tasks = new string[7];
+
+            for(int i = 0; i < days.Length; i++)
+            {
+                Console.WriteLine("Enter a task for " + days[i]);
+                tasks[i] = days[i] + ":  " + Console.ReadLine();
+            }
+
+            foreach(string t in tasks)
+            {
+                Console.WriteLine(t);
+            }
+        }
+    }
+}
